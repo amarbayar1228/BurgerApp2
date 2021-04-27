9b3d767bd2d933f2d48838b94254edf52e798986
amraa <amarbayarbatbayar2@gmail.com>
2021-04-27 14:42:43 +0800

    Updates

---
+++ precache-manifest.e07f01b839f95fcb2d5cf2b23cdf34f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84efecd1ea9e8796f299ec6f2e6c6689",
+    "url": "/BurgerApp2/index.html"
+  },
+  {
+    "revision": "ebb2b60c9fb5cd8a5666",
+    "url": "/BurgerApp2/static/css/main.03eefd3e.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/BurgerApp2/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/BurgerApp2/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb2b60c9fb5cd8a5666",
+    "url": "/BurgerApp2/static/js/main.28bba08f.chunk.js"
+  },
+  {
+    "revision": "52a3d341e37466aa9229",
+    "url": "/BurgerApp2/static/js/runtime-main.4081427b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerApp2/static/media/burger-logo.b8503d26.png"
+  }
+]);
